dc5e503e747b4772e6dfd2fedeada03b3f875c80
Larry Kagan <larry@superiocity.com>
2016-03-07 17:35:27 -0500

    Fix to bypass limited number of posts by other plugin.

--- rchn-glossary.php
+++ rchn-glossary.php
@@ -8,3 +8,41 @@
  * @package Rchn-glossary
  */
 require_once 'post-types/definition.php';
+
+class RCHN_Glossary 
+{
+	protected $wpdb;
+	
+	public function __construct() {
+		$this->wpdb = $GLOBALS['wpdb'];
+		add_shortcode( 'rchn_glossary', array($this, 'list_terms') );
+	}
+	
+	
+	public function list_terms()
+	{
+		$sql = "
+			select post_name, post_title, post_content from {$this->wpdb->prefix}posts
+			where post_status = 'publish'
+				  and post_type = 'definition'
+			order by post_name";
+
+		$rows = $this->wpdb->get_results( $sql );
+		$num_terms = count( $rows );
+		$list = '';
+
+		if ( $num_terms ) {
+			$list .= '<dl class="rchn-definition-list">';
+			
+			for ( $i = 0; $i < $num_terms; ++$i ) {
+				$list .= "<dt>{$rows[$i]->post_title}</dt><dd>{$rows[$i]->post_content}</dd>";
+			}
+
+			$list .= '</dl>';
+		}
+
+		return $list;
+	}
+}
+
+new RCHN_Glossary();
